467489a46e744577f276f78a5a9019cfc33543ee
anas-raza~ <anusraza268@gmail.com>
2024-06-03 06:44:25 -0700

    modified

---
+++ app.js
@@ -0,0 +1,31 @@
+// Variable Scoping
+// Closure
+// Generators
+// Modules
+// Exponentiation Operator
+// Optional chaining
+// Object Methods (keys,values,freez,entries)
+// Destructuring //p
+// Template literals //p
+// Destructuring //p
+// Default parameters //p
+// Rest parameter //p
+// Spread Operator //p
+// Arrow functions  //p
+// Enhanced object literals //p
+// Iterators & For..of  //p
+// Map  //p
+// Array methods  //p
+// Higher-order function  //p
+// CallBack  //p
+// Promises  //p
+// Ternary Operator //p
+
+
+
+
+
+const user={
+    username :'raza',
+    lkl
+}
