0c17f1f68cec03ab9b4cc3d298b31eff79c7b46f
anas-raza~ <anusraza268@gmail.com>
2024-06-03 23:13:41 -0700

    modified

--- app.js
+++ app.js
@@ -25,7 +25,64 @@
 
 
 
-const user={
-    username :'raza',
-    lkl
+// const user={
+//     username :'raza',
+//     email : "anusraza268@gmail.com",
+//     age:20
+// }
+// const {username,email,age}=user
+// console.log(username);
+
+
+// const fruit=['apple','banana','graphes']
+// const[,,two]=fruit
+// console.log(two);
+
+
+// // spreed operator
+// const fruit = ["apple", "banana", "orange", "mango"];
+// const arr2=['water ',...fruit]
+// console.log(arr2);
+// const newArr = arr2.concat(fruit);
+// console.log(newArr);
+
+
+// rest operator
+// function hello(...username) {
+//     console.log(username);
+    
+// }
+// hello("abdullah", "usman", "ammar", "zain", "hassan");
+
+
+
+// ternary operator
+const num=220
+if (num > 50) {
+    console.log('passed');
+}else{
+    console.log('failed');
 }
+
+num >50 ? console.log('passed'):console.log('failed');
+
+
+num>70 ? console.log('bathren'):num> 50 ? console.log('passed'):console.log('failed');
+
+
+// default parmeter
+function greatuser(username='abd') {
+    console.log('hello ${username}');
+    
+}
+greatuser()
+
+
+// Enhanced object literals 
+let name ='abd'
+let age =25
+
+let user={
+    name,age
+}
+console.log(user);
